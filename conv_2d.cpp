a2121e24df4c8d24b8f9ab10bef08475c6a052aa
Sondiem10 <120395796+Sondiem10@users.noreply.github.com>
2025-03-27 17:30:57 +0700

    Update conv_2d.cpp

--- conv_2d.cpp
+++ conv_2d.cpp
@@ -147,6 +147,83 @@ public:
         }
     }
     }
+
+void z_score_normalization(double input[], conv2d_shape in_shape, double epsilon) {
+    int size = in_shape.height * in_shape.width;
+    for (int ic = 0; ic < in_shape.channels; ic++) {
+
+        double mean = 0.0, std_dev = 0.0, sum = 0;
+
+        for (int in_h = 0; in_h < in_shape.height; in_h++) {
+            for (int in_w = 0; in_w < in_shape.width; in_w++) {
+                int input_idx = ic * in_shape.height * in_shape.width + in_h * in_shape.width + in_w;
+                mean += input[input_idx];
+            }
+        }
+
+        double mean = sum / size;
+
+        for (int in_h = 0; in_h < in_shape.height; in_h++) {
+            for (int in_w = 0; in_w < in_shape.height; in_w++) {
+                int input_idx = ic * in_shape.height * in_shape.width + in_h * in_shape.width + in_w;
+                std_dev += (input[input_idx] - mean) * (input[input_idx] - mean);
+            }
+        }
+        std_dev = sqrt(std_dev / size);
+
+        for (int in_h = 0; in_h < in_shape.height; in_h++) {
+            for (int in_w = 0; in_w < in_shape.height; in_w++) {
+                int input_idx = ic * in_shape.height * in_shape.width + in_h * in_shape.width + in_w;
+                input[input_idx] = (input[input_idx] - mean) / (std_dev + epsilon);
+            }
+        }
+
+    }
+}
+void relu( float input[], conv2d_shape in_shape) {
+    for (int i = 0; i < in_shape.channels * in_shape.height * in_shape.width; i++) {
+        input[i] = fmax(0.0, input[i]);
+    }
+}
+struct pool_shape {
+    int height;
+    int width;
+    int depth;
+};
+void maxPooling2D(float input[], float output[], conv2d_shape in_shape, int stride[], pool_shape pool) {
+
+    int out_height = (in_shape.height - pool.height) / stride[0] + 1;
+    int out_width = (in_shape.width - pool.width) / stride[0] + 1;
+    int out_depth = in_shape.channels;
+
+    for (int c = 0; c < out_depth; c++)
+    {
+        for (int h = 0; h < out_height; h++)
+        {
+            for (int w = 0; w < out_width; w++)
+            {
+                int in_start_h = h * stride[0];
+                int in_start_w = w * stride[1];
+                double max_val = input[c * in_shape.height * in_shape.width + in_start_h * in_shape.width + in_start_w];
+                    for (int ph = 0; ph < pool.height; ph++)
+                    {
+                        for (int pw = 0; pw < pool.width; pw++)
+                        {
+                            int in_h = in_start_h + ph;
+                            int in_w = in_start_w + pw;
+                            int input_idx = c * in_shape.height * in_shape.width + in_h * in_shape.width + in_w;
+                            max_val = fmax(max_val, input[input_idx]);
+
+                        }
+                    }
+                int output_idx = c * out_height * out_width + h * out_width + w;
+                output[output_idx] = max_val;
+
+            }
+        }
+    }
+}
+
     conv2d_shape get_output_shape() const { return out_shape; }
 };
 
